e3ecd094d044858b2b1addbf4d0f712678030943
elias89 <eliasjaime.89@gmail.com>
2024-06-20 19:06:30 -0300

    flayout - Refactoring whole lib

--- src/js/index.js
+++ src/js/index.js
@@ -3,53 +3,46 @@ import './../styles/main.scss';
 /*
   TODO: This is still under development, supports only vertical split
 */
-const FL_MIN_PANE_WIDTH = 15;
 
-function flayout(){
-  console.log("flayout!")
-  const restrictMinPaneWidth = (paneWidth) => {
-    return paneWidth > FL_MIN_PANE_WIDTH ? paneWidth : FL_MIN_PANE_WIDTH;
-  }
+const _flayoutContainer = null
+const _flayoutLeftColumn = null;
+const _flayoutRightColumn = null;
+const _flayoutDivider = null;
+let _flayoutIsDragging = false;
 
-  const groups = Array.from(document.querySelectorAll('.flayout-group'));
-  groups.forEach((group) => {
-    const panes = Array.from(group.querySelectorAll('.flayout-pane'));
+function flayout() {
+  _flayoutContainer = document.querySelector('.flayout-main');
+  _flayoutLeftColumn = document.querySelector('.flayout-pane__left');
+  _flayoutRightColumn = document.querySelector('.flayout-pane__right');
+  _flayoutDivider = document.querySelector('.flayout-drag');
 
-    panes[0].style.width = `${group.clientWidth / 2}px`;;
-    panes[1].style.width = `${group.clientWidth / 2}px`;
 
-    const dragElement = document.createElement('div');
-    dragElement.classList.add('flayout-drag');
-    dragElement.setAttribute('draggable', 'true');
-    dragElement.style.left = `${group.offsetWidth / 2}px`;
-    panes[0].after(dragElement);
-
-    dragElement.addEventListener('dragstart', (event) => {
-      event.target.classList.add('dragging')
-    });
-
-    dragElement.addEventListener('drag', (event) => {
-      if (event.clientX <= 0) {
-        return;
-      }
-      const group = event.target.parentElement;
-      const paneA = event.target.previousElementSibling;
-      const paneB = event.target.nextElementSibling;
-
-      let delta = event.clientX - group.offsetLeft;
-
-      paneA.style.width = `${restrictMinPaneWidth(delta)}px`;
-      paneB.style.width = `${restrictMinPaneWidth(group.clientWidth - delta)}px`;
-    });
-
-    dragElement.addEventListener('dragend', (event) => {
-      const paneA = event.target.previousElementSibling;
-      dragElement.style.left = `${paneA.offsetWidth}px`;
-
-      event.target.classList.remove('dragging');
-      paneA.classList.remove('dragging');
-    });
+  _flayoutDivider.addEventListener('mousedown', (e) => {
+    _flayoutIsDragging = true;
+    document.addEventListener('mousemove', flayoutDragStart);
+    document.addEventListener('mouseup', flayoutDragStop);
   });
+}
+
+
+function flayoutDragStart(e) {
+  if (!_flayoutIsDragging) return;
+
+  const containerRect = _flayoutContainer.getBoundingClientRect();
+  const offsetX = e.clientX - containerRect.left;
+
+  const leftColumnWidth = ((offsetX / containerRect.width) * 100);
+  const rightColumnWidth = 100 - leftColumnWidth;
+
+  _flayoutLeftColumn.style.flex = `0 0 ${leftColumnWidth}%`;
+  _flayoutRightColumn.style.flex = `0 0 ${rightColumnWidth}%`;
+  _flayoutDivider.style.left = `${leftColumnWidth}%`;
+}
+
+function flayoutDragStop() {
+  _flayoutIsDragging = false;
+  document.removeEventListener('mousemove', drag);
+  document.removeEventListener('mouseup', stopDrag);
 }
 
 export default flayout;
